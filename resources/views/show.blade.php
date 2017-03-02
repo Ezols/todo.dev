eb0e4687e8a04e7ba48e719e7847a1482d31b0c6
Ezols <Edvards.ozols@gmail.com>
2017-03-02 22:01:28 +0200

    Better list look + back button

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,3 +1,10 @@
 
-{{$user}}
+ID: {{$user->id}}<br/>
+Name: {{$user->name}}<br/>
+Emails: {{$user->email}}<br/>
+Password: {{$user->password}}<br/>
+Tokken: {{$user->remember_token}}<br/>
+Created: {{$user->created_at}}
+
+<a href="{{ url('/user') }}" ><p>BACK</p></a>
 
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,5 +1,5 @@
-
+<p>User list: </p>
 @foreach($users as $user)
     {{-- <a href="{{ action('UsersController@showuser', [$user->id]) }}" ><p>asdasd {{ $user->name }}</p></a> --}}
-    <a href="{{ url('/user', $user->id) }}" ><p>{{ $user->remember_token }} {{ $user->name }}</p></a>
+    <a href="{{ url('/user', $user->id) }}" ><p>{{ $user->name }}</p></a>
 @endforeach
